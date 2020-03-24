04092477c80984bdb1aea47ecdbaa8dc16ea0e7a
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2020-03-24 06:39:30 +0200

    swiper files added

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let openClose = document.querySelector(".openClose");
 const srcOpen = "images/icon-hamburger.svg";
 const srcClose = "images/icon-close.svg";
 let modal = document.querySelector(".nav__modal");
+const body = document.querySelector('body');
 console.log(modal);
 
 openClose.addEventListener("click", function(){
@@ -10,9 +11,11 @@ openClose.addEventListener("click", function(){
       console.log("tears")
       openClose.setAttribute('src', srcClose)
       modal.style.display = "flex";
+      body.style.backgroundColor = ("hsl(227, 12%, 61%)")
     } else 
     {
         openClose.setAttribute('src', srcOpen)
         modal.style.display = "none";
+        body.style.backgroundColor = "transparent";
     }
 })
